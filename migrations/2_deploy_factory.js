c22d5a54ebff68b5d526185e3135493385282e35
Jules Lee <jackconsolidated@gmail.com>
2021-05-07 22:08:03 +0800

    copy migrations and truffle config from apeswap

---
+++ migrations/2_deploy_factory.js
@@ -0,0 +1,10 @@
+const PlantsFactory = artifacts.require("PlantsFactory");
+
+module.exports = function (deployer, network, accounts) {
+  let currentAccount = accounts[0]
+  if(network == 'testnet') {
+    console.warn('WARNING: Using account[1] for testnet')
+    currentAccount = accounts[1]
+  }
+  deployer.deploy(PlantsFactory, currentAccount, {from: currentAccount});
+};
